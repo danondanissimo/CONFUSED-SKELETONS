283d4c24586122cc0368b14fc573e1eecb47c961
danondanissimo <106009630+danondanissimo@users.noreply.github.com>
2024-02-23 14:49:32 +0000

    Added functional for mobile menu buttons

--- src/js/header.js
+++ src/js/header.js
@@ -3,10 +3,27 @@ import { modalBackdrop, closeModalButton } from './modal';
 export const navListItems = document.querySelectorAll('.navigation-list-item');
 export const openModalButton = document.querySelector('.modal-open');
 
-openModalButton.addEventListener('click', () => {
+const toggleMobileMenuButton = document.querySelector('.toggle-mobile-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+export const mobileLogOutButton = document.querySelector(
+  '.mobile-log-out-button'
+);
+
+export const mobileSignUp = document.querySelector('.mobile-sign-up');
+
+function openLoginModal() {
   modalBackdrop.classList.remove('visually-hidden');
   closeModalButton.addEventListener('click', () => {
     modalBackdrop.classList.add('visually-hidden');
   });
   closeModalButton.removeEventListener('click', () => {});
+  mobileSignUp.disabled = true;
+}
+
+openModalButton.addEventListener('click', openLoginModal);
+
+toggleMobileMenuButton.addEventListener('click', () => {
+  mobileMenu.classList.toggle('is-open');
 });
+
+mobileSignUp.addEventListener('click', openLoginModal);

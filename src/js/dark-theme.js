4a22d1919a84fe77edbdbd61796b32d50802b4ee
MyroslavaRogoza <miroslavarogozayyy@gmail.com>
2024-02-24 17:03:40 +0200

    update

--- src/js/dark-theme.js
+++ src/js/dark-theme.js
@@ -1,17 +1,19 @@
 const toggleButton = document.querySelector('.theme-toggle');
 const head = document.querySelector('head');
-const linkElement = document.createElement('link');
-
-
-
 
 toggleButton.addEventListener('click', darkThemeOnButtonClick);
 
-
 function darkThemeOnButtonClick() {
   if (toggleButton.checked) {
+    const linkElement = document.createElement('link');
+    linkElement.setAttribute('rel', 'stylesheet');
+    linkElement.setAttribute('href', '/css/dark-theme.css');
     head.insertAdjacentElement('beforeend', linkElement);
-    console.log('true');
+  } else {
+    const addedElement = head.querySelector('link[href="/css/dark-theme.css"]');
+    console.log(addedElement);
+    if (addedElement) {
+      addedElement.remove();
+    }
   }
 }
-

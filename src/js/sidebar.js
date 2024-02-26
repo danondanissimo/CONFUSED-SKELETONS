c0b8faa9a8e48104d98970befbf3e532a65ca2f1
Olexandr597 <Olexandr597@gmail.com>
2024-02-26 20:50:14 +0200

    up

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -1,9 +1,9 @@
+import { onclickGalleryItem } from '../js/modal-window';
+
 async function getBooks(category) {
   try {
-    let url = 'https://books-backend.p.goit.global/books/all';
-    if (category !== 'ALL CATEGORIES') {
-      url = `https://books-backend.p.goit.global/books/category?category=${category}`;
-    }
+    let url = `https://books-backend.p.goit.global/books/category?category=${category}`;
+
     const response = await fetch(url);
     if (!response.ok) {
       throw new Error('Failed to fetch books');
@@ -24,74 +24,57 @@ async function displayBooks(category) {
     books.forEach(book => {
       const bookInfoHTML = `
                         <div class="book">
-                            <img class="image_book" src="${book.book_image}" alt="Book Image">
+                            <img class="image_book" src="${book.book_image}"  data-id="${book._id}"alt="Book Image">
                             <h2 class="title_book"> ${book.title}</h2>
                             <p class="test_book"> ${book.contributor}</p>
                         </div>
                     `;
       booksList.innerHTML += bookInfoHTML;
     });
+    document.getElementById('selectedCategory').textContent = category;
   } else {
     console.error('Element with class "books-list" not found');
   }
 }
-
-async function displayAllBooks() {
+document.addEventListener('DOMContentLoaded', () => {
   const categoryList = document.getElementById('categoryList');
   if (categoryList) {
-    const categoryItems = categoryList.querySelectorAll('li');
-    categoryItems.forEach(async item => {
-      const category = item.textContent;
-      await displayBooks(category);
+    categoryList.addEventListener('click', event => {
+      if (event.target && event.target.matches('li.category-item')) {
+        let category = event.target.textContent;
+        if (category === 'All Categories') {
+          category = " "; // Якщо обрано "All Categories", передаємо порожній рядок
+        }
+
+        const categoryItems = categoryList.querySelectorAll('.category-item');
+        categoryItems.forEach(item => {
+          item.classList.remove('active');
+        });
+        event.target.classList.add('active');
+        displayBooks(category);
+      }
     });
   } else {
     console.error('Element with id "categoryList" not found');
   }
-}
+});
 
 fetch('https://books-backend.p.goit.global/books/category-list')
   .then(response => response.json())
   .then(data => {
-    // Сортуємо категорії за їх назвами
     data.sort((a, b) => a.list_name.localeCompare(b.list_name));
 
     const categoryList = document.getElementById('categoryList');
 
-    // Додамо категорію "ALL CATEGORIES"
-  
-    const allListItem = document.createElement('li');
-    allListItem.textContent = 'ALL CATEGORIES';
-    allListItem.classList.add('category-item'); // Додаємо клас для взаємодії з CSS
-    allListItem.addEventListener('click', () => {
-      const categoryItems = document.querySelectorAll('.category-item');
-      categoryItems.forEach(item => {
-        item.classList.remove('active');
-      });
-      allListItem.classList.add('active');
-      displayAllBooks();
-    });
-    categoryList.appendChild(allListItem);
-
-    // Додамо інші категорії
-
     data.forEach(item => {
       const category = item.list_name;
-      if (category !== 'ALL CATEGORIES') {
-        const listItem = document.createElement('li');
-        listItem.textContent = category;
-        listItem.classList.add('category-item'); // Додаємо клас для взаємодії з CSS
-        listItem.addEventListener('click', () => {
-          // Забираємо клас 'active' з усіх пунктів списку
-          const categoryItems = categoryList.querySelectorAll('.category-item');
-          categoryItems.forEach(item => {
-            item.classList.remove('active');
-          });
-          // Додаємо клас 'active' до натиснутого пункту
-          listItem.classList.add('active');
-          displayBooks(category);
-        });
-        categoryList.appendChild(listItem);
-      }
+      const listItem = document.createElement('li');
+      listItem.textContent = category;
+      listItem.classList.add('category-item');
+      categoryList.appendChild(listItem);
     });
   })
-  .catch(error => console.error('Помилка отримання даних:', error));
+  .catch(error => console.error('Error fetching data:', error));
+
+const bookList = document.querySelector('.books-list');
+bookList.addEventListener('click', onclickGalleryItem);

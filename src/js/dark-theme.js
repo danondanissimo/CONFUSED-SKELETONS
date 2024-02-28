23e69e302ab1f7c1c7534e50c39f53e32a3d73cd
danondanissimo <106009630+danondanissimo@users.noreply.github.com>
2024-02-28 12:19:38 +0000

    darktheme fix attempt #1

--- src/js/dark-theme.js
+++ src/js/dark-theme.js
@@ -1,6 +1,8 @@
 const toggleButton = document.querySelector('.theme-toggle');
 const head = document.querySelector('head');
 
+import darkTheme from '../css/dark-theme.css';
+
 if (localStorage.getItem('theme') === 'true') {
   toggleButton.checked = 'true';
   toggleTheme();
@@ -18,12 +20,15 @@ function toggleTheme() {
   if (temp) {
     const linkElement = document.createElement('link');
     linkElement.setAttribute('rel', 'stylesheet');
-    linkElement.setAttribute('href', '/css/dark-theme.css');
+    linkElement.setAttribute('href', `${darkTheme}`);
     head.insertAdjacentElement('beforeend', linkElement);
   } else {
-    const addedElement = head.querySelector('link[href="/css/dark-theme.css"]');
+    const addedElement = head.querySelector(`link[href="${darkTheme}"]`);
     if (addedElement) {
       addedElement.remove();
     }
   }
 }
+
+// 'link[href="/css/dark-theme.css"]'
+// '/css/dark-theme.css';

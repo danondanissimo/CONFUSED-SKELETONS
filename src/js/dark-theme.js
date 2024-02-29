b792811f1b95ece553519cd78cc860061f821fe1
danondanissimo <106009630+danondanissimo@users.noreply.github.com>
2024-02-29 12:29:35 +0000

    Enhanced dark theme

--- src/js/dark-theme.js
+++ src/js/dark-theme.js
@@ -1,6 +1,10 @@
 const toggleButton = document.querySelector('.theme-toggle');
 const head = document.querySelector('head');
 
+const root = document.documentElement;
+
+console.log(root);
+
 if (localStorage.getItem('theme') === 'true') {
   toggleButton.checked = 'true';
   toggleTheme();
@@ -17,7 +21,9 @@ function toggleTheme() {
   localStorage.setItem('theme', JSON.stringify(temp));
   if (temp) {
     document.querySelector('body').classList.add('dark');
+    root.classList.add('root-dark');
   } else {
     document.querySelector('body').classList.remove('dark');
+    root.classList.remove('root-dark');
   }
 }
